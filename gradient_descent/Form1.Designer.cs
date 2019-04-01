75aac0b6c126315c795798719cc313223dcb97e4
Carl1016 <410335011@gms.ndhu.edu.tw>
2019-04-01 16:04:43 +0800

    加入專案檔案。

---
+++ gradient_descent/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace gradient_descent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(59, 50);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(559, 295);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(717, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(717, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(714, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "you can chose arbitrary num for x";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ gradient_descent/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gradient_descent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double origin_slope;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.chart1.Series[1].Points.Clear();
+            double x = Convert.ToDouble(textBox1.Text);
+            Draw_model();
+            origin_slope = Count_slope(x, x-0.1, find_y_in_model(x), find_y_in_model(x-0.1));
+            GD(x, x-0.1, find_y_in_model(x), find_y_in_model(x-0.1), origin_slope, 0);
+            
+        }
+        public void Draw_model()
+        {
+            double y = 0 ;
+            
+            for (double x=0;x<=10;x=x+0.01)
+            {
+                y = find_y_in_model(x);
+                this.chart1.Series[0].Points.AddXY(x, y);
+            }
+        }
+        public double find_y_in_model(double x)
+        {
+            const double e = 2.71828182846;
+            double y = 0;
+            y = 1 - x * Math.Pow(e, -x);
+            return y;
+        }
+        public void GD(double current_x, double last_x, double current_y, double last_y,double slope,int num)
+        {
+            if (slope == 0 || num == 30000 || find_dx(current_x, last_x, current_y, last_y)==0)
+            {
+
+            }
+            else
+            {
+                this.chart1.Series[1].Points.AddXY(current_x, current_y);
+                num++;
+                double dx = find_dx(current_x, last_x, current_y, last_y);
+                double next_x = current_x+dx;
+                GD(next_x, current_x, find_y_in_model(next_x), find_y_in_model(current_x),Count_slope(next_x, current_x, find_y_in_model(next_x), find_y_in_model(current_x)),num);
+
+            }
+        }
+        public double find_dx(double current_x, double last_x, double current_y, double last_y)
+        {
+            double slope = Count_slope(current_x, last_x, current_y, last_y);
+            double dx = 0;
+            dx = -0.2 * slope;
+            return dx;
+        }
+        public double Count_slope(double current_x,double last_x,double current_y,double last_y)
+        {
+            double slope;
+            slope = (current_y - last_y) / (current_x - last_x);
+            return slope;
+        }
+    }
+}
